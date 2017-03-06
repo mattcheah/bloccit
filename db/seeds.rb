0e24f2fd41c61a8284159b17291738682e896612
Matt Cheah <matt.cheah@gmail.com>
2017-03-06 17:23:39 +0000

    create seed file and seed db with 50 random posts and 100 random comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'random_data'
+
+#create posts
+
+50.times do 
+   Post.create!(
+         title: RandomData.random_sentence,
+         body: RandomData.random_paragraph
+   )
+end
+posts = Post.all
+
+#Create Comments
+
+100.times do
+   Comment.create!(
+      post:posts.sample,
+      body: RandomData.random_paragraph
+   )
+end
+
+puts "Seed Finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+
+
