47ea614b52ece120f5eae25842ab7064635464f6
Matt Cheah <matt.cheah@gmail.com>
2017-03-08 19:17:16 +0000

    add functionality for update and delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    #@comments = Comments.where(id: params[:id])
   end
 
   def new
@@ -27,5 +28,35 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+  
+  def update 
+    @post = Post.find(params[:id])
+    
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    
+    if @post.save
+      flash[:notice] = "Post was edited"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error editing the post. Please try again"
+      render :edit
+    end
+  end
+  
+  def destroy
+     @post = Post.find(params[:id])
+ 
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to posts_path
+     else
+       flash.now[:alert] = "There was an error deleting the post."
+       render :show
+     end
+  end
+    
+  
 end
