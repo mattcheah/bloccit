57bd96e86b51dbaac9bbd48e8a13f68621678dc2
Matt Cheah <matt.cheah@gmail.com>
2017-03-13 17:57:51 +0000

    create user model with routes and testing, and name, email, pw validation

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'bootstrap-sass'
-
+gem 'bcrypt'
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309213205) do
+ActiveRecord::Schema.define(version: 20170311023135) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 20170309213205) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
